77344ee088988d242349b429aaea9be07829d3b3
lupiel <50728722+lupiel@users.noreply.github.com>
2023-09-07 12:16:19 +0200

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_instance" "terraform" {
+  name         = "terraform"
+  machine_type = "e2-standard-2"
+  tags         = ["web", "dev"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
