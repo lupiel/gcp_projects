302d2a4a86c1e13e4fe36b1427651c4567b2852d
lupiel <50728722+lupiel@users.noreply.github.com>
2023-09-22 14:33:37 +0200

    Update main.tf

--- terraform/tfinfra_3_states/main.tf
+++ terraform/tfinfra_3_states/main.tf
@@ -4,20 +4,21 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "test-bucket-for-state" {
-  name        = "Project ID"
+  name        = "<bucket_name>"
   location    = "US"
   uniform_bucket_level_access = true
   force_destroy = true # terraform destroy
 }
 
 terraform {
+  # select one backend 
   backend "local" {
     path = "terraform/state/terraform.tfstate"
   }
-  backend "gcs" {
-    bucket  = "Project ID"
-    prefix  = "terraform/state"
-  }
+  # backend "gcs" {
+  #   bucket  = "<bucket_name>"
+  #   prefix  = "terraform/state"
+  # }
 }
 
 
